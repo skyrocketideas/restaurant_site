9c348550a21b1158cddd43bdfd9e040db310be5e
Graeme Clark <grae0026@stud.kea.dk>
2019-09-06 13:56:12 +0200

    Import JS from previous version

---
+++ js/restaurant_site.js
@@ -0,0 +1,77 @@
+const modal = document.querySelector(".modal-background");
+modal.addEventListener("click", () => {
+    modal.classList.add("hide");
+});
+
+//fetch categories from API
+fetch("https://kea-alt-del.dk/t5/api/categories")
+    .then(res => res.json())
+    .then(function (data) {
+        data.forEach(buildCategory)
+        getProducts();
+    })
+
+//build category with header and section
+//create id
+//add category to main
+function buildCategory(data) {
+    const section = document.createElement("section");
+    const header = document.createElement("h1");
+    header.textContent = data;
+    section.setAttribute("id", data)
+    section.appendChild(header);
+    document.querySelector("main").appendChild(section);
+}
+
+//create function
+//fetch products from API
+function getProducts() {
+    fetch("https://kea-alt-del.dk/t5/api/productlist")
+        .then(function (response) {
+            return response.json()
+        }).then(function (data) {
+            data.forEach(showDish)
+        })
+}
+
+function showDish(dish) {
+    const template = document.querySelector("template").content;
+    const copy = template.cloneNode(true);
+    copy.querySelector(".data_name").textContent = dish.name;
+    copy.querySelector(".data_price").textContent = `${dish.price} kr`;
+
+    copy.querySelector(".data_price").classList.add("discount");
+
+    copy.querySelector(".data_alcohol").textContent = `Alcohol Units = ${dish.alcohol}`;
+    copy.querySelector(".data_image").src = `imgs/small/${dish.image}-sm.jpg`;
+
+    //if dish is on discount
+    if (dish.discount) {
+        copy.querySelector(".data_price").classList.add("discount");
+        copy.querySelector(".data_discount").textContent = Math.round(dish.price - dish.discount / 100 * dish.price) + "kr"
+
+    } else {
+        copy.querySelector(".data_discount").remove();
+    }
+
+    //if dish is not sold out
+    if (!dish.soldout) {
+        copy.querySelector("article").classList.remove("soldOut");
+    }
+
+    copy.querySelector("button").addEventListener("click", () => {
+        fetch(`https://kea-alt-del.dk/t5/api/product?id=${dish.id}`)
+            .then(res => res.json())
+            .then(showDetails);
+    });
+
+    //add dish to category
+    document.querySelector(`#${dish.category}`).appendChild(copy);
+}
+
+// the modal
+function showDetails(data) {
+    modal.querySelector(".modal-name").textContent = data.name;
+    modal.querySelector(".modal-description").textContent = data.longdescription;
+    modal.classList.remove("hide");
+}
